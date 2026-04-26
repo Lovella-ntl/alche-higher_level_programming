1932b2df86cc8dd88a410cf01344cfec877b4dc6
Lovella-ntl <l.nkusi@alustudent.com>
2026-04-26 05:35:28 -0700

    updated 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(s):
     for char in s:
         if ord('a') <= ord(char) <= ord('z'):
-            char = chr(ord(char) - ord('a') + ord('A'))
+            print(chr(ord(char) - ord('a') + ord('A')), end="")
         else:
             print(char, end="")
     print()
