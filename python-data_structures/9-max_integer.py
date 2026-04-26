fe6fcf4cdd4dd3b1f6df12c57b8cca08c8ff2b55
Lovella-ntl <l.nkusi@alustudent.com>
2026-04-26 11:21:02 -0700

    updated 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -7,5 +7,5 @@ def max_integer(my_list=[]):
     max_number = my_list[0]
     for number in range(len(my_list)):
         if my_list[number] > max_number:
-            max_number =  my_list[number]
+            max_number = my_list[number]
     return max_number
