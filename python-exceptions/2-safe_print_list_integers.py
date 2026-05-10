b4babd48172932d8595faf95809d4e843132fbfc
Lovella.ntl <l.nkusi@alustudent.com>
2026-05-10 18:20:17 +0400

    updated 2-safe_p4rint_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,19 +2,14 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    printed_elts = 0
+    count = 0
 
-    try:
-        for v in my_list:
-            try:
-                int_value = int(v)
-                print("{:d}".format(int_value), end="")
-                printed_elts += 1
-                if printed_elts >= x:
-                    break
-            except ValueError:
-                continue
-    except TypeError:
-        pass
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (IndexError, ValueError, TypeError):
+            pass
+
     print()
-    return (printed_elts)
+    return count
